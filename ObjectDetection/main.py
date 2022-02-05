74f0f5ca55ea997923ea96c78d691c5d90db9491
heesangh <heesang@vt.edu>
2022-02-05 14:09:55 -0500

    color object detection for stationary images

--- ObjectDetection/main.py
+++ ObjectDetection/main.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 
 image = cv2.imread("buoy.jpg")
-cv2.imshow('image', image)
+cv2.imshow('Original', image)
 cv2.waitKey(1)
 
 hsvimg = cv2.cvtColor(image, cv2.COLOR_BGR2HSV);
-cv2.imshow('new', hsvimg)
+cv2.imshow('HSV', hsvimg)
 cv2.namedWindow('Track')
 
 #trackbar
 def track(x):
     pass
-cv2.createTrackbar('Hmin', 'Track', 0, 179, track);
+cv2.createTrackbar('Hmin', 'Track', 150, 179, track);
 cv2.createTrackbar('Hmax', 'Track', 179, 179, track);
 cv2.createTrackbar('Smin', 'Track', 0, 255, track);
 cv2.createTrackbar('Smax', 'Track', 255, 255, track);
@@ -20,6 +20,8 @@ cv2.createTrackbar('Vmin', 'Track', 0, 255, track);
 cv2.createTrackbar('Vmax', 'Track', 255, 255, track);
 
 while True:
+
+    image = cv2.imread("buoy.jpg");
     h_min = cv2.getTrackbarPos('Hmin', 'Track')
     h_max = cv2.getTrackbarPos('Hmax', 'Track')
     s_min = cv2.getTrackbarPos('Smin', 'Track')
@@ -33,14 +35,26 @@ while True:
     mask = cv2.inRange(hsvimg, lower, upper)
     cv2.imshow('Mask', mask)
 
+    contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    lists = list(contours)
+    lists.sort(key=cv2.contourArea, reverse=True)
+
+    red_area = lists[0]
+    (xg, yg, wg, hg) = cv2.boundingRect(red_area)
+    # cv2.rectangle(image, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 3)
+    cv2.drawContours(image, red_area, -1, (0, 255, 0), 3)
+    cv2.putText(image, "Area: " + str(cv2.contourArea(red_area)), (xg, yg), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
+
+    print(cv2.contourArea(red_area))
+
+    red_area2 = lists[1]
+    (xg, yg, wg, hg) = cv2.boundingRect(red_area2)
+
+    cv2.drawContours(image, red_area2, -1, (0, 255, 0), 3)
+    print(cv2.contourArea(red_area2))
+    cv2.putText(image, "Area: " + str(cv2.contourArea(red_area2)), (xg, yg), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
+
+
+    cv2.imshow('Original', image);
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    # draw rectangle
-    #cn = cv2.findContours(hsvimg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #if (len(cn)) > 0:
-     #   red_area = max(cn, key=cv2.contourArea);
-      #  (xg, yg, wg, hg) = cv2.boundingRect(red_area);
-       # cv2.rectangle(image, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 3)
-        #print('Obj DEC')
-
