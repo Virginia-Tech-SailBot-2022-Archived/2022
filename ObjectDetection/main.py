06cf4723933a5b45d672f5217815409c50662cb8
heesangh <heesang@vt.edu>
2022-02-05 15:30:40 -0500

    color object detection for webcam

--- ObjectDetection/main.py
+++ ObjectDetection/main.py
@@ -1,27 +1,35 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("buoy.jpg")
-cv2.imshow('Original', image)
-cv2.waitKey(1)
+from threading import Thread
 
-hsvimg = cv2.cvtColor(image, cv2.COLOR_BGR2HSV);
-cv2.imshow('HSV', hsvimg)
+# image = cv2.imread("buoy.jpg")
+#cv2.imshow('Original', image)
+# cv2.waitKey(1)
+
+#hsvimg = cv2.cvtColor(image, cv2.COLOR_BGR2HSV);
+#cv2.imshow('HSV', hsvimg)
 cv2.namedWindow('Track')
 
 #trackbar
 def track(x):
     pass
-cv2.createTrackbar('Hmin', 'Track', 150, 179, track);
-cv2.createTrackbar('Hmax', 'Track', 179, 179, track);
+cv2.createTrackbar('Hmin', 'Track', 0, 179, track);
+cv2.createTrackbar('Hmax', 'Track', 10, 179, track);
 cv2.createTrackbar('Smin', 'Track', 0, 255, track);
 cv2.createTrackbar('Smax', 'Track', 255, 255, track);
 cv2.createTrackbar('Vmin', 'Track', 0, 255, track);
 cv2.createTrackbar('Vmax', 'Track', 255, 255, track);
 
-while True:
+cap = cv2.VideoCapture(0)
 
-    image = cv2.imread("buoy.jpg");
+while True:
+    #webcam
+
+    _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #image = cv2.imread("buoy.jpg");
     h_min = cv2.getTrackbarPos('Hmin', 'Track')
     h_max = cv2.getTrackbarPos('Hmax', 'Track')
     s_min = cv2.getTrackbarPos('Smin', 'Track')
@@ -32,29 +40,36 @@ while True:
 
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
-    mask = cv2.inRange(hsvimg, lower, upper)
-    cv2.imshow('Mask', mask)
+    #mask = cv2.inRange(hsvimg, lower, upper)
+    mask2 = cv2.inRange(hsv_frame, lower, upper)
+    cv2.imshow('Mask', mask2)
 
-    contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     lists = list(contours)
     lists.sort(key=cv2.contourArea, reverse=True)
 
     red_area = lists[0]
     (xg, yg, wg, hg) = cv2.boundingRect(red_area)
-    # cv2.rectangle(image, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 3)
-    cv2.drawContours(image, red_area, -1, (0, 255, 0), 3)
-    cv2.putText(image, "Area: " + str(cv2.contourArea(red_area)), (xg, yg), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
+    cv2.rectangle(frame, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 3)
+    #cv2.drawContours(frame, red_area, -1, (0, 255, 0), 3)
+    #cv2.putText(frame, "Area: " + str(cv2.contourArea(red_area)), (xg, yg), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
 
-    print(cv2.contourArea(red_area))
+    print(str(wg*hg))
 
     red_area2 = lists[1]
     (xg, yg, wg, hg) = cv2.boundingRect(red_area2)
+    cv2.rectangle(frame, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 3)
+    #cv2.drawContours(frame, red_area2, -1, (0, 255, 0), 3)
+    #print(cv2.contourArea(red_area2))
+    #cv2.putText(frame, "Area: " + str(cv2.contourArea(red_area2)), (xg, yg), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
 
-    cv2.drawContours(image, red_area2, -1, (0, 255, 0), 3)
-    print(cv2.contourArea(red_area2))
-    cv2.putText(image, "Area: " + str(cv2.contourArea(red_area2)), (xg, yg), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
+    cv2.imshow('Video', frame)
 
 
-    cv2.imshow('Original', image);
+
+    #cv2.imshow('Original', image);
+
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
